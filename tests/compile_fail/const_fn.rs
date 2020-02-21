2dade5af324dc660491b1bc30649a592b0f1c54a
Eli Dupree <vcs@elidupree.com>
2020-02-21 17:03:33 -0500

    tested `const fn` and `async fn` error messages

---
+++ tests/compile_fail/const_fn.rs
@@ -0,0 +1,14 @@
+use live_prop_test::live_prop_test;
+
+#[live_prop_test(test_five)]
+const fn five() -> i32 {
+  5
+}
+
+fn test_five() -> impl FnOnce(&i32) {
+  move |result| {
+    assert_eq!(*result, 5);
+  }
+}
+
+fn main() {}
--- tests/compile_failures.rs
+++ tests/compile_failures.rs
@@ -2,4 +2,6 @@
 fn tests() {
   let tester = trybuild::TestCases::new();
   tester.compile_fail("tests/compile_fail/wildcard_argument.rs");
+  tester.compile_fail("tests/compile_fail/const_fn.rs");
+  tester.compile_fail("tests/compile_fail/async_fn.rs");
 }
