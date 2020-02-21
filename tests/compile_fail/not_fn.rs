bf55015e07f097f39d195b65860c6c4de8643d0e
Eli Dupree <vcs@elidupree.com>
2020-02-21 17:42:38 -0500

    tested applying the macro to a non-fn

---
+++ tests/compile_fail/not_fn.rs
@@ -0,0 +1,6 @@
+use live_prop_test::live_prop_test;
+
+#[live_prop_test]
+struct NotAFunction;
+
+fn main() {}
--- tests/compile_failures.rs
+++ tests/compile_failures.rs
@@ -4,4 +4,5 @@ fn tests() {
   tester.compile_fail("tests/compile_fail/wildcard_argument.rs");
   tester.compile_fail("tests/compile_fail/const_fn.rs");
   tester.compile_fail("tests/compile_fail/async_fn.rs");
+  tester.compile_fail("tests/compile_fail/not_fn.rs");
 }
