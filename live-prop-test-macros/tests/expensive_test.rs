a2f00ade8da6942bc4781dbda25f9b04c6572e95
Eli Dupree <vcs@elidupree.com>
2020-02-22 17:47:38 -0500

    Made a test of the randomly not calling expensive tests behavior

---
+++ live-prop-test-macros/tests/expensive_test.rs
@@ -0,0 +1,33 @@
+use live_prop_test_macros::live_prop_test;
+
+use std::cell::RefCell;
+use std::time::{Duration, Instant};
+
+struct TestTracker {
+  runs: u64,
+}
+
+#[live_prop_test(expensive_test)]
+fn tested_function(_tracker: &RefCell<TestTracker>) {}
+
+fn expensive_test<'a>(tracker: &RefCell<TestTracker>) -> impl FnOnce(&()) + 'a {
+  tracker.borrow_mut().runs += 1;
+  move |_| {
+    std::thread::sleep(Duration::from_micros(50));
+  }
+}
+
+#[test]
+fn expensive_test_runs_a_reasonable_number_of_times() {
+  let tracker = RefCell::new(TestTracker { runs: 0 });
+  let start_time = Instant::now();
+  let mut function_calls = 0;
+  while start_time.elapsed() < Duration::from_millis(5) {
+    tested_function(&tracker);
+    function_calls += 1;
+  }
+  let runs = tracker.borrow().runs;
+  assert!(function_calls >= 200);
+  assert!(runs >= 3);
+  assert!(runs <= 50);
+}
