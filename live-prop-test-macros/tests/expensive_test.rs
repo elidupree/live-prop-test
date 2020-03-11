fe4aeff66e3b16815c3183ab28cdc5678988602c
Eli Dupree <vcs@elidupree.com>
2020-03-11 15:56:24 -0400

    added another test for the throttling (which fails)

--- live-prop-test-macros/tests/expensive_test.rs
+++ live-prop-test-macros/tests/expensive_test.rs
@@ -9,19 +9,31 @@ struct TestTracker {
   runs: Vec<u64>,
 }
 
+// Don't use std::thread::sleep for testing, because it's imprecise
+fn busy_wait(duration: Duration) {
+  let start_time = Instant::now();
+  while start_time.elapsed() < duration {}
+  println!(
+    "Busy-waited {} micros out of expected {}",
+    start_time.elapsed().as_secs_f64() * 1_000_000.0,
+    duration.as_secs_f64() * 1_000_000.0
+  );
+}
+
 #[live_prop_test(expensive_test)]
-fn tested_function(tracker: &RefCell<TestTracker>) {
+fn function_with_expensive_test(tracker: &RefCell<TestTracker>, _test_micros: u64) {
   tracker.borrow_mut().calls += 1;
 }
 
 fn expensive_test<'a>(
   tracker: &RefCell<TestTracker>,
+  test_micros: &'a u64,
 ) -> impl FnOnce(&()) -> Result<(), String> + 'a {
   let mut tracker = tracker.borrow_mut();
   let calls = tracker.calls;
   tracker.runs.push(calls);
   move |_| {
-    std::thread::sleep(Duration::from_micros(50));
+    busy_wait(Duration::from_micros(*test_micros));
     Ok(())
   }
 }
@@ -34,7 +46,7 @@ fn expensive_test_runs_a_reasonable_number_of_times() {
   });
   let start_time = Instant::now();
   while start_time.elapsed() < Duration::from_millis(5) {
-    tested_function(&tracker);
+    function_with_expensive_test(&tracker, 50);
   }
   let runs = tracker.borrow().runs.len();
   assert!(tracker.borrow().calls >= 200);
@@ -51,11 +63,29 @@ fn expensive_test_doesnt_run_the_same_every_time() {
     });
     let start_time = Instant::now();
     while start_time.elapsed() < Duration::from_millis(5) {
-      tested_function(&tracker);
+      function_with_expensive_test(&tracker, 50);
     }
     tracker.into_inner().runs
   }
 
   let first = runs();
   assert!((0..10).map(|_| runs()).any(|runs| runs != first));
+}
+
+#[test]
+fn cheap_test_runs_every_time() {
+  let tracker = RefCell::new(TestTracker {
+    calls: 0,
+    runs: Vec::new(),
+  });
+  let start_time = Instant::now();
+  while start_time.elapsed() < Duration::from_millis(5) {
+    function_with_expensive_test(&tracker, 20);
+    busy_wait(Duration::from_micros(500));
+    println!("{:?}", start_time.elapsed());
+  }
+  let runs = tracker.borrow().runs.len() as u64;
+  assert_eq!(runs, tracker.borrow().calls);
+  assert!(runs >= 3);
+  assert!(runs <= 50);
 }
