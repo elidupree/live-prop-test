bfb49f2ca58e3e9424909587b7f1b0a18fbe99c0
Eli Dupree <vcs@elidupree.com>
2020-08-22 11:29:24 -0400

    add another check about the debt tracking

--- live-prop-test-macros/tests/expensive_test_runs_a_reasonable_number_of_times.rs
+++ live-prop-test-macros/tests/expensive_test_runs_a_reasonable_number_of_times.rs
@@ -17,12 +17,15 @@ fn expensive_test_runs_a_reasonable_number_of_times() {
     live_prop_test::mock_sleep(Duration::from_micros(5));
     function_with_expensive_test(&tracker, 50);
   }
-  let runs = tracker.borrow().runs.len();
+  let runs = &tracker.borrow().runs;
   assert!(tracker.borrow().calls == 10000);
 
   // total time taken without tests is 50 ms
   // so we should expect to spend around 10% of that on testing, i.e. 5 ms
   // which is 100 tests
-  assert!(runs >= 50);
-  assert!(runs <= 200);
+  assert!(runs.len() >= 50);
+  assert!(runs.len() <= 200);
+
+  // make sure it didn't simply deplete its usage in the first half and then not have any left for the second half
+  assert!(runs.iter().filter(|run| **run > 5000).count() * 4 >= runs.len())
 }
